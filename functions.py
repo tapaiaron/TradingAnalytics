8cd88335d929b354cc412305bd5e2b423ada5d6a
tapaiaron <aron.tapai@gmail.com>
2023-04-23 00:15:42 +0200

    Fixing errors

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 import modules
-
+from modules import datetime, yf
 
 def load_financial_data(tickerSymbol='META', start_date='2014-01-01', end_date=(datetime.date.today()-datetime.timedelta(days=1)).strftime('%Y-%m-%d')):
     tickerData=yf.Ticker(tickerSymbol)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import modules
 import functions
 
 def main():
-    data = functions.load_data()
+    data = functions.load_financial_data()
 
 if __name__ == "__main__":
     main()
