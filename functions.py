ed3db6f85c6e3eb1da2baaf069a7ba87616debd3
tapaiaron <aron.tapai@gmail.com>
2023-04-23 00:11:10 +0200

    Setting up basic stuff.

--- functions.py
+++ functions.py
@@ -1,4 +1,8 @@
 import modules
 
-def load_data():
-    print("loaded")
+
+def load_financial_data(tickerSymbol='META', start_date='2014-01-01', end_date=(datetime.date.today()-datetime.timedelta(days=1)).strftime('%Y-%m-%d')):
+    tickerData=yf.Ticker(tickerSymbol)
+    df=tickerData.history(period='1d', start=start_date, end=end_date)
+    print(df)
+
