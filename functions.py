4d1ef5a6e049a4b3b8fea1810b394b91ea42a47c
Aron Tapai <70382136+tapaiaron@users.noreply.github.com>
2023-04-23 16:46:29 +0200

    Delete functions.py

--- functions.py
+++
@@ -1,8 +0,0 @@
-import modules
-from modules import datetime, yf
-
-def load_financial_data(tickerSymbol='META', start_date='2014-01-01', end_date=(datetime.date.today()-datetime.timedelta(days=1)).strftime('%Y-%m-%d')):
-    tickerData=yf.Ticker(tickerSymbol)
-    df=tickerData.history(period='1d', start=start_date, end=end_date)
-    print(df)
-
